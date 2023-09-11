ecc514e85e439c3b2295bb87f1965cdc161df45a
Ömer <omer670067@gmail.com>
2023-09-11 16:11:46 +0300

    API düzenlemesi

--- kac_kisiyiz_api/src/connector.ts
+++ kac_kisiyiz_api/src/connector.ts
@@ -20,8 +20,9 @@ export class Connector {
   }
 
   middlewares() {
-    this.app.use(express.json());
     this.app.use(morgan("dev"));
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
   }
 
   private async listen() {
@@ -50,9 +51,11 @@ export class Connector {
       "CREATE TABLE IF NOT EXISTS users (id INT PRIMARY KEY AUTO_INCREMENT, mail VARCHAR(255), password VARCHAR(255), name VARCHAR(255), money INT, onesignalId VARCHAR(255));" +
       "CREATE TABLE IF NOT EXISTS bank (id INT PRIMARY KEY AUTO_INCREMENT, userId VARCHAR(255), nameSurname VARCHAR(255), bankName VARCHAR(255), iban VARCHAR(30));" +
       "CREATE TABLE IF NOT EXISTS categories (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), icon VARCHAR(20));" +
-      "CREATE TABLE IF NOT EXISTS surveys (id INT PRIMARY KEY AUTO_INCREMENT, categoryId INT, userId INT, title VARCHAR(255), content VARCHAR(1000), image VARCHAR(250), ch1 INT, ch2 INT, adLink VARCHAR(255), isRewarded BOOL);" +
+      "CREATE TABLE IF NOT EXISTS surveys (id INT PRIMARY KEY AUTO_INCREMENT, categoryId INT, userId INT, title VARCHAR(255), content VARCHAR(1000), image VARCHAR(250), ch1 INT, ch2 INT, adLink VARCHAR(255), isRewarded BOOL, isPending BOOL);" +
       "CREATE TABLE IF NOT EXISTS pending (id INT PRIMARY KEY AUTO_INCREMENT, surveyId INT, userId INT, category VARCHAR(255), title VARCHAR(255), content VARCHAR(1000));" +
-      "CREATE TABLE IF NOT EXISTS settings (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), attr VARCHAR(2000));";
+      "CREATE TABLE IF NOT EXISTS settings (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), attr VARCHAR(2000));" +
+      "CREATE TABLE IF NOT EXISTS voted (id INT PRIMARY KEY AUTO_INCREMENT, surveyId INT, userId INT, vote VARCHAR(3));" +
+      "CREATE TABLE IF NOT EXISTS rewarded (id INT PRIMARY KEY AUTO_INCREMENT, surveyId INT, reward DECIMAL(10, 2));";
     await this.con.query(sql, (err) => {
       if (err) throw err;
     });
